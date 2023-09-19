d9ef3e04fc33fa2e653da00ee4eeb595f4eb546c
Panagiotis Deligiannis <deligianp@athenarc.gr>
2023-09-19 12:16:39 +0300

    Add trailing / to url declaration for uploads

--- files/urls.py
+++ files/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from files.views import UploadAPIView
 
 urlpatterns = [
-    path(r'upload', UploadAPIView.as_view(), name='uploads'),
+    path(r'upload/', UploadAPIView.as_view(), name='uploads'),
 ]
