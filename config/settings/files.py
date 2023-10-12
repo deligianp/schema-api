7a5e1f919b3a545525fefa6aa47d5df536d761c6
Panagiotis Deligiannis <deligianp@athenarc.gr>
2023-10-12 11:22:48 +0300

    Fix default value for S3 URL environment variable

--- config/settings/files.py
+++ config/settings/files.py
@@ -1,10 +1,11 @@
 import json
+from urllib.parse import urlparse
 
 from config.env import env
 
 USE_FILES = env.bool('SCHEMA_API_USE_FILES', False)
 S3 = {
-    'URL': env.url('SCHEMA_API_S3_URL', None).geturl() or None,
+    'URL': env.url('SCHEMA_API_S3_URL', '').geturl() or None,
     'ACCESS_KEY_ID': env.str('SCHEMA_API_S3_ACCESS_KEY_ID', None),
     'SECRET_ACCESS_KEY': env.str('SCHEMA_API_S3_SECRET_ACCESS_KEY', None),
     'VALIDITY_PERIOD_SECONDS': env.int('SCHEMA_API_S3_VALIDITY_PERIOD_SECONDS', 24 * 60 * 60),
