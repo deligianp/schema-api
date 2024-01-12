8b87425967134c5621ad11c7092b9d142f895516
Panagiotis Deligiannis <deligianp@athenarc.gr>
2024-01-12 10:48:27 +0200

    Fix env for Django ALLOWED_HOSTS setting

--- config/environments/dev.py
+++ config/environments/dev.py
@@ -1,12 +1,12 @@
 from config.environments.base import *
 
 DEBUG = True
-ALLOWED_HOSTS = env.list('SCHEMA_API_ALLOWED_HOSTS', default='*')
+ALLOWED_HOSTS = env.list('SCHEMA_API_ALLOWED_HOSTS', default=['*'])
 SECRET_KEY = env.str('SECRET_KEY', None)
 if not SECRET_KEY:
     SECRET_KEY_PATH = env.str('SCHEMA_API_SECRET_KEY_FILE', None)
     if not SECRET_KEY_PATH:
-        SECRET_KEY='insecure-secret-key'
+        SECRET_KEY = 'insecure-secret-key'
     else:
         with open(SECRET_KEY_PATH, 'r', encoding='utf-8') as secret_key_file:
             SECRET_KEY = secret_key_file.read().strip()
