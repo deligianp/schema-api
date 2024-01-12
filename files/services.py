47379b77febe821a973ea7624be90a3cffc9e2c6
Panagiotis Deligiannis <deligianp@athenarc.gr>
2024-01-12 10:52:42 +0200

    Refactor UploadService to manage
    - Use set environment variables related to the use of SSL for S3
    requests originating from SCHEMA api.
    - Instantiate S3 client during instance construction

--- files/services.py
+++ files/services.py
@@ -28,10 +28,9 @@ class UploadService:
                                       aws_access_key_id=settings.S3['ACCESS_KEY_ID'],
                                       aws_secret_access_key=settings.S3['SECRET_ACCESS_KEY'],
                                       config=boto3.session.Config(signature_version='s3v4'),
-                                      verify=False,
-                                      use_ssl=False
+                                      verify=settings.S3['USE_SSL'],
+                                      use_ssl=settings.S3['USE_SSL']
                                       )
-        self._create_bucket_if_not_exists()
 
     def create_upload_request(self, size: int, file_path: str):
         validity_period_seconds = settings.S3['VALIDITY_PERIOD_SECONDS']
@@ -42,34 +41,28 @@ class UploadService:
         key = file_path
         expiry = current_ref_ts + validity_period
 
-        s3_target = boto3.client('s3',
-                                 endpoint_url=settings.S3['URL'],
-                                 aws_access_key_id=settings.S3['ACCESS_KEY_ID'],
-                                 aws_secret_access_key=settings.S3['SECRET_ACCESS_KEY'],
-                                 config=boto3.session.Config(signature_version='s3v4'),
-                                 verify=False,
-                                 use_ssl=False
-                                 )
-
         max_part_size = settings.S3['MAX_PART_SIZE_BYTES']
+
+        self._create_bucket_if_not_exists()
+
         if size > max_part_size:
             # Calculate sizes
             part_sizes = [max_part_size] * (size // max_part_size) + [size % max_part_size]
 
-            # Issue new multipart upload creation
-            response = s3_target.create_multipart_upload(Bucket=bucket, Key=key, Expires=expiry)
+            # Issue new multipart upload creation=
+            response = self.s3_client.create_multipart_upload(Bucket=bucket, Key=key, Expires=expiry)
 
             upload_id = response['UploadId']
 
             urls = []
             for i in range(len(part_sizes)):
-                url = s3_target.generate_presigned_url(ClientMethod='upload_part',
+                url = self.s3_client.generate_presigned_url(ClientMethod='upload_part',
                                                        Params={'Bucket': bucket, 'Key': key, 'PartNumber': i + 1,
                                                                'UploadId': upload_id, 'ContentLength': part_sizes[i]},
                                                        ExpiresIn=validity_period_seconds)
                 urls.append({'part': i + 1, 'url': url, 'n_bytes': part_sizes[i]})
 
-            complete_url = s3_target.generate_presigned_url(ClientMethod='complete_multipart_upload',
+            complete_url = self.s3_client.generate_presigned_url(ClientMethod='complete_multipart_upload',
                                                             Params={'Bucket': bucket, 'Key': key,
                                                                     'UploadId': upload_id},
                                                             ExpiresIn=validity_period_seconds
@@ -82,7 +75,7 @@ class UploadService:
                     'finalize': complete_url
                 }
             }
-        url = s3_target.generate_presigned_url(ClientMethod='put_object',
+        url = self.s3_client.generate_presigned_url(ClientMethod='put_object',
                                                Params={'Bucket': bucket, 'Key': key, 'ContentLength': size},
                                                ExpiresIn=validity_period_seconds)
         return {
