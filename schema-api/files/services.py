2dba103a0695390009ce1263cd938d62f90baef3
Panagiotis Deligiannis <deligianp@athenarc.gr>
2024-01-12 15:53:10 +0200

    Infer object storage bucket name from auth entity UUID

--- schema-api/files/services.py
+++ schema-api/files/services.py
@@ -17,11 +17,8 @@ class UploadService:
             raise ApplicationError(
                 f'{self.__class__.__name__} depends on an AuthEntity of type {AuthEntityType.USER}'
             )
-        elif auth_entity.profile is None:
-            raise ApplicationError(
-                f'{self.__class__.__name__} requires the existence of a profile for the AuthEntity user dependency'
-            )
         self.auth_entity = auth_entity
+        self.bucket = str(self.auth_entity.uuid)
 
         self.s3_client = boto3.client('s3',
                                       endpoint_url=settings.S3['URL'],
@@ -37,7 +34,6 @@ class UploadService:
         validity_period = datetime.timedelta(seconds=validity_period_seconds)
 
         current_ref_ts = datetime.datetime.now()
-        bucket = self.auth_entity.profile.fs_user_dir
         key = file_path
         expiry = current_ref_ts + validity_period
 
@@ -50,20 +46,20 @@ class UploadService:
             part_sizes = [max_part_size] * (size // max_part_size) + [size % max_part_size]
 
             # Issue new multipart upload creation=
-            response = self.s3_client.create_multipart_upload(Bucket=bucket, Key=key, Expires=expiry)
+            response = self.s3_client.create_multipart_upload(Bucket=self.bucket, Key=key, Expires=expiry)
 
             upload_id = response['UploadId']
 
             urls = []
             for i in range(len(part_sizes)):
                 url = self.s3_client.generate_presigned_url(ClientMethod='upload_part',
-                                                       Params={'Bucket': bucket, 'Key': key, 'PartNumber': i + 1,
+                                                       Params={'Bucket': self.bucket, 'Key': key, 'PartNumber': i + 1,
                                                                'UploadId': upload_id, 'ContentLength': part_sizes[i]},
                                                        ExpiresIn=validity_period_seconds)
                 urls.append({'part': i + 1, 'url': url, 'n_bytes': part_sizes[i]})
 
             complete_url = self.s3_client.generate_presigned_url(ClientMethod='complete_multipart_upload',
-                                                            Params={'Bucket': bucket, 'Key': key,
+                                                            Params={'Bucket': self.bucket, 'Key': key,
                                                                     'UploadId': upload_id},
                                                             ExpiresIn=validity_period_seconds
                                                             )
@@ -76,7 +72,7 @@ class UploadService:
                 }
             }
         url = self.s3_client.generate_presigned_url(ClientMethod='put_object',
-                                               Params={'Bucket': bucket, 'Key': key, 'ContentLength': size},
+                                               Params={'Bucket': self.bucket, 'Key': key, 'ContentLength': size},
                                                ExpiresIn=validity_period_seconds)
         return {
             'type': 'simple',
@@ -85,14 +81,13 @@ class UploadService:
         }
 
     def create_download_request(self, file_path: str):
-        bucket = self.auth_entity.profile.fs_user_dir
         key = file_path
         validity_period_seconds = settings.S3['VALIDITY_PERIOD_SECONDS']
         validity_period = datetime.timedelta(seconds=validity_period_seconds)
         current_ref_ts = datetime.datetime.now()
         expiry = current_ref_ts + validity_period
         try:
-            self.s3_client.head_object(Bucket=bucket, Key=key)
+            self.s3_client.head_object(Bucket=self.bucket, Key=key)
         except ClientError as ex:
             if ex.response['Error']['Code'] == 'NotFound':
                 raise NotFound
@@ -101,17 +96,16 @@ class UploadService:
             'expiry': expiry,
             'url': self.s3_client.generate_presigned_url(
                 ClientMethod='get_object',
-                Params={'Bucket': bucket, 'Key': key},
+                Params={'Bucket': self.bucket, 'Key': key},
                 ExpiresIn=validity_period_seconds
             )
         }
 
     def _create_bucket_if_not_exists(self):
-        bucket = self.auth_entity.profile.fs_user_dir
         try:
-            self.s3_client.head_bucket(Bucket=bucket)
+            self.s3_client.head_bucket(Bucket=self.bucket)
         except ClientError as ex:
             if ex.response['Error']['Code'] == '404':
-                self.s3_client.create_bucket(Bucket=bucket)
+                self.s3_client.create_bucket(Bucket=self.bucket)
             else:
                 raise
