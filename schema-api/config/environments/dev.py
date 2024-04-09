2f0693c650c77e492f40fb2640808d5047840a03
Panagiotis Deligiannis <deligianp@athenarc.gr>
2024-04-09 14:53:07 +0300

    Set up logging for stream and files
    - Steam logging is always enabled
    - File logging uses a rotating file scheme

--- schema-api/config/environments/dev.py
+++ schema-api/config/environments/dev.py
@@ -22,3 +22,63 @@ else:
     DATABASES = {
         'default': SQLITE_DATABASE_CONFIGURATION
     }
+
+LOGGING_CONF = {
+    "FILES": {
+        "DIRECTORY": env.str('SCHEMA_API_LOGS_DIRECTORY', None),
+        'FILENAME': env.str('SCHEMA_API_LOGS_ACTIVITY_FILE_NAME', 'schema-api'),
+        'ERROR_FILENAME': env.str('SCHEMA_API_LOGS_ERROR_FILE_NAME', None),
+        'BYTES': env.int('SCHEMA_API_LOGS_FILE_SIZE_BYTES', 10 * 1024 * 1024),
+        'BACKUP_COUNT': env.int('SCHEMA_API_LOGS_BACKUP_COUNT', 10)
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'base': {
+            'format': '[{levelname}][{asctime}]: {message}',
+            'style': '{'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'base'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
+if LOGGING_CONF["FILES"]["DIRECTORY"]:
+    LOGGING['handlers']['file'] = {
+        'level': 'DEBUG',
+        'class': 'logging.handlers.RotatingFileHandler',
+        'filename': f'{os.path.join(LOGGING_CONF["FILES"]["DIRECTORY"], LOGGING_CONF["FILES"]["FILENAME"])}.log',
+        'maxBytes': LOGGING_CONF["FILES"]["BYTES"],
+        'backupCount': LOGGING_CONF["FILES"]["BACKUP_COUNT"],
+        'formatter': 'base'
+    }
+    LOGGING['handlers']['error-file'] = {
+        'level': 'ERROR',
+        'class': 'logging.handlers.RotatingFileHandler',
+        'filename': f'{os.path.join(LOGGING_CONF["FILES"]["DIRECTORY"], LOGGING_CONF["FILES"]["ERROR_FILENAME"] or LOGGING_CONF["FILES"]["FILENAME"] + "-error")}.log',
+        'maxBytes': LOGGING_CONF["FILES"]["BYTES"],
+        'backupCount': LOGGING_CONF["FILES"]["BACKUP_COUNT"],
+        'formatter': 'base'
+    }
+    LOGGING['loggers']['']['handlers'].extend(['file', 'error-file'])
+    LOGGING['loggers']['django']['handlers'].extend(['file', 'error-file'])
