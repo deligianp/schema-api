3439e77ecd13e5c41b03e8210bbd60fb78ebf09b
Panagiotis Deligiannis <deligianp@gmail.com>
2024-04-17 23:08:52 +0300

    Add corsheaders to installed apps

--- schema-api/config/environments/base.py
+++ schema-api/config/environments/base.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'django_filters',
     'api_auth',
