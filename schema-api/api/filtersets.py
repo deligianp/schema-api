595ff4b0fac395fc319686e57cd2d1cc69bb60d6
Panagiotis Deligiannis <deligianp@athenarc.gr>
2024-05-30 09:13:57 +0300

    Support ordering for listing of tasks

--- schema-api/api/filtersets.py
+++ schema-api/api/filtersets.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from api.constants import TaskStatus
@@ -8,4 +9,16 @@ class TaskFilter(filters.FilterSet):
     status = filters.MultipleChoiceFilter(choices=TaskStatus.choices)
     after = filters.DateTimeFilter(field_name='submitted_at', lookup_expr='gte')
     before = filters.DateTimeFilter(field_name='submitted_at', lookup_expr='lt')
+    order = filters.OrderingFilter(
+        fields=(
+            ('uuid', 'uuid'),
+            ('status', 'status'),
+            ('submitted_at', 'submitted_at')
+        )
+    )
+    search = filters.CharFilter(method='filter_by_search')
 
+    def filter_by_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(uuid__icontains=value) | Q(name__icontains=value)
+        )
