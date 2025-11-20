8154277dbd0d97ef68f85702553918e2be837840
deligianp <deligianp@gmail.com>
2025-11-20 14:58:22 +0200

    Expose experiment workflow management on API

--- schema-api/experiments/urls.py
+++ schema-api/experiments/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from experiments.views import ExperimentTasksAPIView, ExperimentDetailsAPIView, ExperimentsAPIView
+from experiments.views import ExperimentTasksAPIView, ExperimentDetailsAPIView, ExperimentsAPIView, \
+    ExperimentWorkflowsAPIView
 
 urlpatterns = [
     path(r'experiments', ExperimentsAPIView.as_view(), name='experiments'),
     path(r'experiments/<username>/<name>', ExperimentDetailsAPIView.as_view(), name='experiment-details'),
     path(r'experiments/<username>/<name>/tasks', ExperimentTasksAPIView.as_view(), name='experiment-tasks'),
+    path(r'experiments/<username>/<name>/workflows', ExperimentWorkflowsAPIView.as_view(), name='experiment-workflows'),
 ]
--- schema-api/experiments/views.py
+++ schema-api/experiments/views.py
@@ -11,8 +11,10 @@ from api_auth.auth import ApiTokenAuthentication
 from api_auth.permissions import IsUser, IsActive, IsContextMember
 from api_auth.services import AuthEntityService
 from experiments.serializers import ExperimentsListSerializer, ExperimentSerializer, ExperimentUpdateSerializer
-from experiments.services import ExperimentService, ExperimentTaskService
+from experiments.services import ExperimentService, ExperimentTaskService, ExperimentWorkflowService
 from util.exceptions import ApplicationValidationError, ApplicationNotFoundError
+from workflows.serializers import WorkflowsBasicListSerializer
+from workflows.services import WorkflowService
 
 
 # Create your views here.
@@ -436,8 +438,8 @@ class ExperimentTasksAPIView(APIView):
         auth_entity_service = AuthEntityService(request.user.parent)
         creator = auth_entity_service.get_user(username)
 
-        if not request.data:
-            raise ApplicationValidationError('No task UUIDs were provided in request body')
+        # if not request.data:
+        #     raise ApplicationValidationError('No task UUIDs were provided in request body')
 
         task_uuids = set(request.data)
         task_service = TaskService(context=request.context, auth_entity=creator)
@@ -526,4 +528,184 @@ class ExperimentTasksAPIView(APIView):
 
         tasks = experiment_task_service.get_tasks()
         output_serializer = TasksBasicListSerializer(tasks, many=True)
+        return Response(status=status.HTTP_200_OK, data=output_serializer.data)
+
+
+class ExperimentWorkflowsAPIView(APIView):
+    authentication_classes = [ApiTokenAuthentication] if settings.USE_AUTH else []
+    permission_classes = [IsAuthenticated, IsUser, IsActive, IsContextMember] if settings.USE_AUTH else []
+
+    @extend_schema(
+        summary='Set experiment workflows',
+        description='Set experiment workflows by workflow UUID',
+        tags=['Experiments'],
+        request=True,
+        examples=[
+            OpenApiExample(
+                'set-experiment-no-workflows',
+                summary='Set experiment to have no workflows',
+                value=[],
+                request_only=True,
+                response_only=False,
+            ),
+            OpenApiExample(
+                'set-experiment-workflows',
+                summary='Set experiment workflows',
+                value=[
+                    "551ec417-e034-4c3e-811b-69cba5cb377a",
+                    "4883e205-274c-4f16-8fb6-b46199d759a5",
+                    "6132f1ce-b557-4f43-852d-873252721ae1"
+                ],
+                request_only=True,
+                response_only=False,
+            )
+        ],
+        responses={
+            202: OpenApiResponse(
+                description='Experiment was found and workflow list was updated',
+                response=TasksBasicListSerializer,
+                examples=[
+                    OpenApiExample(
+                        'updated-experiment-workflows-list',
+                        summary='Updated experiment workflows list',
+                        value=[
+                            {
+                                "uuid": "551ec417-e034-4c3e-811b-69cba5cb377a",
+                                "name": "foo_task",
+                                "state": {
+                                    "status": "APPROVED",
+                                    "updated_at": "2024-11-13T11:49:06.320683Z"
+                                }
+                            },
+                            {
+                                "uuid": "4883e205-274c-4f16-8fb6-b46199d759a5",
+                                "name": "foo_task",
+                                "state": {
+                                    "status": "APPROVED",
+                                    "updated_at": "2024-11-14T06:22:40.766514Z"
+                                }
+                            }
+                        ],
+                        request_only=False,
+                        response_only=True,
+                    ),
+                    OpenApiExample(
+                        'updated-experiment-workflows-list-with-no-tasks',
+                        summary='Updated experiment workflows list with no tasks',
+                        value=[],
+                        request_only=False,
+                        response_only=True,
+                    )
+                ]
+            ),
+            400: OpenApiResponse(
+                description='Invalid experiment data'
+            ),
+            401: OpenApiResponse(
+                description='Invalid credentials or credentials were not provided.'
+            ),
+            403: OpenApiResponse(
+                description='Insufficient permissions'
+            ),
+            404: OpenApiResponse(
+                description='Either user is not a context participant, experiment not found in context or a referenced '
+                            'workflows does not exist in context'
+            )
+        }
+    )
+    def put(self, request, username: str, name: str):
+        auth_entity_service = AuthEntityService(request.user.parent)
+        creator = auth_entity_service.get_user(username)
+
+        # if not request.data:
+        #     raise ApplicationValidationError('No workflow UUIDs were provided in request body')
+
+        workflow_uuids = set(request.data)
+        workflow_service = WorkflowService(context=request.context, user=creator)
+
+        workflows = workflow_service.get_workflows().filter(uuid__in=workflow_uuids)
+        if len(workflow_uuids) != len(workflows):
+            indicative_uuid = workflow_uuids.difference(set(str(w.uuid) for w in workflows)).pop()
+
+            raise ApplicationNotFoundError(f'No workflow with UUID "{indicative_uuid}" exists in context '
+                                           f'"{request.context.name}"')
+
+        experiment_service = ExperimentService(request.context)
+        experiment = experiment_service.retrieve_experiment(name, creator)
+
+        experiment_workflow_service = ExperimentWorkflowService(experiment)
+        experiment_workflow_service.set_workflows(workflows)
+
+        # TODO:
+        output_serializer = WorkflowsBasicListSerializer(experiment_workflow_service.get_workflows(), many=True)
+        return Response(status=status.HTTP_202_ACCEPTED, data=output_serializer.data)
+
+    @extend_schema(
+        summary='Get experiment tasks',
+        description='Get experiment tasks',
+        tags=['Experiments'],
+        responses={
+            200: OpenApiResponse(
+                description='Experiment was found and task list was updated',
+                response=TasksBasicListSerializer,
+                examples=[
+                    OpenApiExample(
+                        'experiment-tasks-list',
+                        summary='Experiment tasks list',
+                        value=[
+                            {
+                                "uuid": "551ec417-e034-4c3e-811b-69cba5cb377a",
+                                "name": "foo_task",
+                                "state": {
+                                    "status": "APPROVED",
+                                    "updated_at": "2024-11-13T11:49:06.320683Z"
+                                }
+                            },
+                            {
+                                "uuid": "4883e205-274c-4f16-8fb6-b46199d759a5",
+                                "name": "foo_task",
+                                "state": {
+                                    "status": "APPROVED",
+                                    "updated_at": "2024-11-14T06:22:40.766514Z"
+                                }
+                            }
+                        ],
+                        request_only=False,
+                        response_only=True,
+                    ),
+                    OpenApiExample(
+                        'Experiment-tasks-list-with-no-tasks',
+                        summary='Experiment tasks list with no tasks',
+                        value=[],
+                        request_only=False,
+                        response_only=True,
+                    )
+                ]
+            ),
+            400: OpenApiResponse(
+                description='Invalid experiment data'
+            ),
+            401: OpenApiResponse(
+                description='Invalid credentials or credentials were not provided.'
+            ),
+            403: OpenApiResponse(
+                description='Insufficient permissions'
+            ),
+            404: OpenApiResponse(
+                description='Either user is not a context participant, experiment not found in context or a referenced '
+                            'task does not exist in context'
+            )
+        }
+    )
+    def get(self, request, username: str, name: str):
+        auth_entity_service = AuthEntityService(request.user.parent)
+        creator = auth_entity_service.get_user(username)
+
+        experiment_service = ExperimentService(request.context)
+        experiment = experiment_service.retrieve_experiment(name, creator)
+
+        experiment_workflow_service = ExperimentWorkflowService(experiment)
+
+        workflows = experiment_workflow_service.get_workflows()
+        output_serializer = WorkflowsBasicListSerializer(workflows, many=True)
         return Response(status=status.HTTP_200_OK, data=output_serializer.data)
